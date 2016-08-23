2ed0adee035fbaacb0f656dc86da35c69bfc7670
kairat_kushaev <kkushaev@mirantis.com>
2016-08-23 18:13:57 +0300

    Do some cleanup on Glare
    Some piece of code is not needed anymore or already have
    implemented in oslo. So we can remove it from Glare codebase.
    
    Change-Id: I80cd87723b7c12a29f34bbbb62e5e2f437e5ab47

--- glare/tests/utils.py
+++ glare/tests/utils.py
@@ -26,6 +26,7 @@ import fixtures
 from oslo_config import cfg
 from oslo_config import fixture as cfg_fixture
 from oslo_log import log
+from oslo_middleware import base as base_middleware
 import six
 from six.moves import BaseHTTPServer
 import testtools
@@ -33,7 +34,6 @@ import webob
 
 from glare.common import config
 from glare.common import utils
-from glare.common import wsgi
 from glare import context
 
 CONF = cfg.CONF
@@ -441,13 +441,10 @@ def start_http_server(image_id, image_data):
         return pid, port
 
 
-class FakeAuthMiddleware(wsgi.Middleware):
+class FakeAuthMiddleware(base_middleware.ConfigurableMiddleware):
 
-    def __init__(self, app, is_admin=False):
-        super(FakeAuthMiddleware, self).__init__(app)
-        self.is_admin = is_admin
-
-    def process_request(self, req):
+    @staticmethod
+    def process_request(req):
         auth_token = req.headers.get('X-Auth-Token')
         user = None
         tenant = None
@@ -465,7 +462,7 @@ class FakeAuthMiddleware(wsgi.Middleware):
             'user': user,
             'tenant': tenant,
             'roles': roles,
-            'is_admin': self.is_admin,
+            'is_admin': False,
             'auth_token': auth_token,
         }
 
