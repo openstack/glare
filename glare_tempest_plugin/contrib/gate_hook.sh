a91d198974467da7dad936354a1f8aeb7ba379e7
Mike Fedosin <mikhail.fedosin.ext@nokia.com>
2017-07-28 23:04:13 +0300

    [DNM] Test gate
    Change-Id: Ia4d8014b04c5a39b59f6af8f87e9195cd93e5e80

--- glare_tempest_plugin/contrib/gate_hook.sh
+++ glare_tempest_plugin/contrib/gate_hook.sh
@@ -12,3 +12,12 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+
+echo "gate_hook.sh executed"
+
+export DEVSTACK_GATE_TEMPEST=1
+export KEEP_LOCALRC=1
+
+GATE_DEST=$BASE/new
+DEVSTACK_PATH=$GATE_DEST/devstack
+$GATE_DEST/devstack-gate/devstack-vm-gate.sh
