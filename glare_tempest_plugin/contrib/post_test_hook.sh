dcfdcc93c10b298677044da7144574677896097a
Mike Fedosin <mikhail.fedosin.ext@nokia.com>
2017-07-28 23:04:13 +0300

    [DNM] Test gate
    Change-Id: Ia4d8014b04c5a39b59f6af8f87e9195cd93e5e80

--- glare_tempest_plugin/contrib/post_test_hook.sh
+++ glare_tempest_plugin/contrib/post_test_hook.sh
@@ -12,3 +12,21 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+
+set -ex
+
+sudo chmod -R a+rw /opt/stack/
+(cd $BASE/new/tempest/; sudo virtualenv .venv)
+source $BASE/new/tempest/.venv/bin/activate
+
+(cd $BASE/new/tempest/; sudo pip install -r requirements.txt -r test-requirements.txt)
+
+sudo cp $BASE/new/tempest/etc/logging.conf.sample $BASE/new/tempest/etc/logging.conf
+
+(cd $BASE/new/glare/; sudo pip install -r requirements.txt -r test-requirements.txt)
+(cd $BASE/new/glare/; sudo python setup.py install)
+
+export TOX_TESTENV_PASSENV=ZUUL_PROJECT
+(cd $BASE/new/tempest/; sudo -E tox -eall-plugin glare)
+
+echo "post_test_hook.sh executed"
