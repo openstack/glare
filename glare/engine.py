f1c659f74c1b13b239b5c11461fe09356eb10f6c
Mike Fedosin <mikhail.fedosin.ext@nokia.com>
2017-09-07 19:23:14 +0300

    Run upload hooks in separate threads
    To prevent possible freezes in upload hook due to cpu intensive tasks
    we should execute it in a separate thread.
    
    Change-Id: Id3556d0d6f1973c7b9755428d2587822beddd70f

--- glare/engine.py
+++ glare/engine.py
@@ -15,6 +15,7 @@
 
 from copy import deepcopy
 
+from eventlet import tpool
 import jsonpatch
 from oslo_config import cfg
 from oslo_log import log as logging
@@ -574,9 +575,10 @@ class Engine(object):
                 if hasattr(af, 'validate_upload'):
                     LOG.warning("Method 'validate_upload' was deprecated. "
                                 "Please use 'pre_upload_hook' instead.")
-                    fd, path = af.validate_upload(context, af, field_name, fd)
+                    fd, path = tpool.execute(
+                        af.validate_upload, context, af, field_name, fd)
                 else:
-                    fd = af.pre_upload_hook(
+                    fd = tpool.execute(af.pre_upload_hook,
                         context, af, field_name, blob_key, fd)
             except exception.GlareException:
                 raise
